17a195a0477e3b72adf404f30b4dc76cf625c18c
debuconnor <gterppw@gmail.com>
2022-03-30 00:52:46 +0900

    Create LinkButton.svelte

---
+++ src/modules/button/LinkButton.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { currentUrl } from '@lib/stores';
+    export let text;
+    export let goto;
+
+    const link = `${currentUrl}${goto}`;
+</script>
+<a href={link}>{text}</a>
+
+<style>
+    a{
+        display: inline-block;
+        padding: 20px;
+        background-color: black;
+        color: white;
+        cursor: pointer;
+        text-decoration: none;
+    }
+</style>
