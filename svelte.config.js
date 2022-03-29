255ca7e3b2b5717faaf08d13fc0c7ccca08608b7
debuconnor <gterppw@gmail.com>
2022-03-29 21:58:02 +0900

    Setup Sass

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,36 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		vite: {
+			resolve: {
+				alias: {
+					'@modules': path.resolve('./src/modules'),
+					'@lib': path.resolve('./src/lib')
+				}
+			},
+
+			css: {
+				preprocessorOptions: {
+					scss: {
+						additionalData: '@use "src/variables.scss" as *;'
+					}
+				}
+			}
+		}
+	},
+
+	preprocess: [
+		preprocess({
+			scss: {
+				prependData: '@use "src/variables.scss" as *;'
+			}
+		})
+	]
 };
 
 export default config;
